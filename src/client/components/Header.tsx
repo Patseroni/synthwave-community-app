7179110e078b856bcda53954aa7d80eac354744a
Patseroni <patrik.bustad@lerniastudent.se>
2026-01-12 19:20:47 +0100

    install and apply react-router-dom and add css-file for header

--- src/client/components/Header.tsx
+++ src/client/components/Header.tsx
@@ -1,7 +1,14 @@
+import Menu from './Menu.tsx'
+import styles from '../styles/Header.module.css'
+
 function Header() {
     return (
         <>
-            <h1>Retrowave Music Community</h1>
+            <div className={styles.header}>
+                <h1 className={styles.headerHeading}>Retrowave Music Community</h1>
+                <Menu />
+            </div>
+
         </>
     )
 
--- src/client/components/Menu.tsx
+++ src/client/components/Menu.tsx
@@ -1,10 +1,15 @@
+import { Link } from "react-router-dom";
+
 function Menu() {
     return (
-
         <>
             <nav>
-                <Link to '/index'></Link>
-        </nav >
+                <Link to='/profile'>Profile</Link>
+                <Link to='/register'>Register</Link>
+                <Link to='/login'>Log in</Link>
+            </nav >
         </>
     )
 }
+
+export default Menu
